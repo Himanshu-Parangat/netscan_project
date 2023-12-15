a50d87d9fe6612a6ac24e9280d398d7935b9ef4d
Himanshu-Parangat <himanshuparangat005@gmail.com>
2023-12-15 21:47:22 +0530

    cleanup& setup route

--- app/run.py
+++ app/run.py
@@ -10,6 +10,18 @@ def index():
     return render_template("index.html")
 
 
+# @app.route('/user/<username>')
+# def show_user(username):
+#     # Assuming you have a templates folder with a file named 'user.html'
+#     return render_template('dashboard.html', username=username)
+
+
+# landing page and aurthintication 
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+
 # Run the Flask app if this script is the main program
 if __name__ == '__main__':
     app.run()
